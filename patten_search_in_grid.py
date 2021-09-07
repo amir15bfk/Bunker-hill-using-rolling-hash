4f94754279b8ca59eddfeaec672909aea6d1aed1
amir <amir15bfk@gmail.com>
2021-09-07 18:48:24 +0100

    work on y

--- patten_search_in_grid.py
+++ patten_search_in_grid.py
@@ -7,6 +7,7 @@ class Grid():
         self.p = p
         self.current = self.hash(self.grid)
         self.hp = self.hash(self.p)
+        self.done = False
     def hash(self,x):
         return sum(sum(x[i][j] for i in range(len(self.p))) for j in range(len(self.p[0])))
     def match(self):
@@ -17,19 +18,25 @@ class Grid():
         else:
             return False
     def next(self):
-        if self.x+len(self.p)<len(self.grid) and 0<=self.x:
-            for i in range(len(self.p[0])):
-                self.current -= self.mw*self.grid[self.x][self.y+i]
-                self.current += self.mw*self.grid[self.x+len(self.p)][self.y+i]
-            self.x+=self.mw
-        else:
-            self.next_y()
-            self.x -= self.mw
-            self.mw*=-1
+        if not self.done:
+            if self.x+len(self.p)<len(self.grid) and 0<=self.x:
+                for i in range(len(self.p[0])):
+                    self.current -= self.mw*self.grid[self.x][self.y+i]
+                    self.current += self.mw*self.grid[self.x+len(self.p)][self.y+i]
+                self.x+=self.mw
+            else:
+                self.next_y()
+                self.x -= self.mw
+                self.mw*=-1
             
-
     def next_y(self):
-        pass
+        if self.y+len(self.p[0])<len(self.grid):
+            for i in range(len(self.p)):
+                self.current -= self.grid[self.x+i][self.y]
+                self.current += self.grid[self.x+i][self.y+len(self.p[1])]
+            self.y+=1
+        else:
+            self.done = True
 
 
 
