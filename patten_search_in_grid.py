3f98214fd6e3e776fc57c747e10072eb44f2a449
amir <amir15bfk@gmail.com>
2021-09-07 18:38:26 +0100

    moving in x is work

--- patten_search_in_grid.py
+++ patten_search_in_grid.py
@@ -3,11 +3,12 @@ class Grid():
         self.grid = grid
         self.x = 0
         self.y = 0
-        self.current = hash(self.grid)
+        self.mw = 1
         self.p = p
-        self.hp = hash(self.p)
-    def hash(x):
-        return sum(sum(x[i][j] for i in range(3)) for j in range(3))
+        self.current = self.hash(self.grid)
+        self.hp = self.hash(self.p)
+    def hash(self,x):
+        return sum(sum(x[i][j] for i in range(len(self.p))) for j in range(len(self.p[0])))
     def match(self):
         if self.current == self.hp:
             #TODO : real check of equality
@@ -16,8 +17,35 @@ class Grid():
         else:
             return False
     def next(self):
-        for i in range(3):
-            self.current -= self.grid[self.x][self.y-i]
-            self.current += self.grid[self.x+3][self.y-i]
+        if self.x+len(self.p)<len(self.grid) and 0<=self.x:
+            for i in range(len(self.p[0])):
+                self.current -= self.mw*self.grid[self.x][self.y+i]
+                self.current += self.mw*self.grid[self.x+len(self.p)][self.y+i]
+            self.x+=self.mw
+        else:
+            self.next_y()
+            self.x -= self.mw
+            self.mw*=-1
+            
 
-x= Grid([[1,2,1],[1,2,1],[1,2,1],[1,2,1]])
+    def next_y(self):
+        pass
+
+
+
+x= Grid([[1,2,1,1],[1,2,1,1],[1,2,1,1],[1,3,1,1]],[[1,2,1],[1,2,1],[1,2,1]])
+print(x.match())
+x.next()
+print(x.match())
+x.next()
+print(x.match())
+x.next()
+print(x.match())
+x.next()
+print(x.match())
+x.next()
+print(x.match())
+x.next()
+print(x.match())
+x.next()
+print(x.match())
