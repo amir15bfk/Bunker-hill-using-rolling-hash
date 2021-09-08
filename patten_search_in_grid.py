4190e942490ec0c64154d7bcd92e13f48bc4c63f
amir <amir15bfk@gmail.com>
2021-09-08 09:07:48 +0100

    Done

--- patten_search_in_grid.py
+++ patten_search_in_grid.py
@@ -1,19 +1,9 @@
 class Grid():
-    def __init__(self,grid,p):
+    def __init__(self,grid):
         self.grid = grid
-        self.x = 0
-        self.y = 0
-        self.mw = 1
-        self.p = p
-        self.current = self.hash(self.grid)
-        self.hp = self.hash(self.p)
-        self.done = False
-        self.count = 0 
-        self.where = []
-        self.not_col_done = True
     def hash(self,x):
         return sum(sum(x[i][j] for i in range(len(self.p))) for j in range(len(self.p[0])))
-    def match(self):
+    def __match(self):
         if self.current == self.hp:
             #TODO : real check of equality
             for i in range(len(self.p)):
@@ -22,80 +12,89 @@ class Grid():
                         return False
 
             if 1:
-                self.count += 0 
+                self.count += 1 
                 self.where.append((self.x,self.y))
                 return True
         else:
             return False
-    def next(self):
+    def __next(self):
         if not self.done:
-            if self.x+len(self.p)<len(self.grid) and 0<=self.x and self.not_col_done:
-                for i in range(len(self.p[0])):
-                    self.current -= self.mw*self.grid[self.x][self.y+i]
-                    self.current += self.mw*self.grid[self.x+len(self.p)][self.y+i]
-                if self.x+len(self.p)<len(self.grid) and self.mw ==1:
-                    self.x+=self.mw
-                else:
-                    self.not_col_done = False
-                    self.mw*=-1 
+            if (self.mw == 1 and (self.x+len(self.p))==len(self.grid))or(self.mw == -1 and (self.x==0)):
+                self.__next_y()
+                self.mw*=-1 
             else:
-                self.next_y()
-                self.not_col_done = True
+                if self.mw == 1:
+                    if ((self.x+len(self.p))<len(self.grid)):
+                        for i in range(len(self.p[0])):
+                            self.current -= self.grid[self.x][self.y+i]
+                            self.current += self.grid[self.x+len(self.p)][self.y+i]
+                else:
+                    if (0<self.x):
+                        for i in range(len(self.p[0])):
+                            self.current += self.grid[self.x-1][self.y+i]
+                            self.current -= self.grid[self.x+len(self.p)-1][self.y+i]
+            
+                self.x+=self.mw
                 
     def draw(self):
         for i in range(len(self.p)):
                 for j in range(len(self.p[0])):
                     print(self.grid[self.x+i][self.y+j],end=" ")
                 print()       
-    def next_y(self):
+    def __next_y(self):
         if self.y+len(self.p[0])<len(self.grid[0]):
             for i in range(len(self.p)):
                 self.current -= self.grid[self.x+i][self.y]
                 self.current += self.grid[self.x+i][self.y+len(self.p[0])]
             self.y+=1
-        # else:
-        #     self.done = True
+        else:
+            self.done = True
+    def run(self,p):
+        self.p = p
+        self.x = 0
+        self.y = 0
+        self.mw = 1
+        self.current = self.hash(self.grid)
+        self.hp = self.hash(self.p)
+        self.done = False
+        self.count = 0 
+        self.where = []
+        while not self.done:
+            self.__match()
+            self.__next()
+        return self.count,self.where
 
 
 
 x=Grid([[1,2,1,1,2,1],
         [1,2,1,1,2,1],
         [1,2,1,1,2,1],
-        [1,3,1,1,2,1]],[[1,2,1],[1,2,1],[1,2,1]])
-print(x.match())
-x.draw()
-x.next()
-print(x.match())
-x.draw()
-x.next()
-print(x.match())
-x.draw()
-x.next()
-print(x.match())
-x.draw()
-x.next()
-print(x.match())
-x.draw()
-x.next()
-print(x.match())
-x.draw()
-x.next()
-print(x.match())
-x.draw()
-x.next()
-print(x.match())
-x.draw()
-x.next()
-print(x.match())
-x.draw()
-x.next()
-print(x.match())
-x.draw()
-x.next()
-print(x.match())
-x.draw()
-x.next()
-print(x.match())
-x.draw()
-print(x.count)
-print(x.where)
+        [1,3,1,1,2,1],
+        [1,2,1,1,2,1],
+        [1,2,1,1,2,1],
+        [1,2,1,1,2,1],
+        [1,3,1,1,2,1],
+        [1,2,1,1,2,1],
+        [1,2,1,1,2,1],
+        [1,2,1,1,2,1],
+        [1,3,1,1,2,1],
+        [1,2,1,1,2,1],
+        [1,2,1,1,2,1],
+        [1,2,1,1,2,1],
+        [1,3,1,1,2,1],
+        [1,2,1,1,2,1],
+        [1,2,1,1,2,1],
+        [1,2,1,1,2,1],
+        [1,3,1,1,2,1],
+        [1,2,1,1,2,1],
+        [1,2,1,1,2,1],
+        [1,2,1,1,2,1],
+        [1,3,1,1,2,1],
+        [1,2,1,1,2,1],
+        [1,2,1,1,2,1],
+        [1,2,1,1,2,1],
+        [1,3,1,1,2,1]])
+p = [[2,1,1,2],[2,1,1,2],[2,1,1,2]] 
+c,w= x.run(p)
+print(c)
+print(w)
